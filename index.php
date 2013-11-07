e252b1cafcfc51c1618e1440e9d5d436f44691a6
Feast Creative <support@feastcreative.com>
2013-11-07 10:09:27 +0000

    added title

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 <head>
 <meta charset="UTF-8">
 <title>Untitled Document</title>
+<title>GIT TEST</title>
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
 <? echo '<h1>Hello world</h1>' ?>
+<? echo '<h1>Hello world.</h1>' ?>
 
 </body>
 </html>
