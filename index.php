6bc6ba60b657701ce5f884a328838b72c7826225
Feast Creative <support@feastcreative.com>
2013-11-07 10:10:09 +0000

    test

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Untitled Document</title>
 <title>GIT TEST</title>
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
-<? echo '<h1>Hello world</h1>' ?>
 <? echo '<h1>Hello world.</h1>' ?>
+
+<h2>This is a not good idea.</h2>
 
 </body>
 </html>
